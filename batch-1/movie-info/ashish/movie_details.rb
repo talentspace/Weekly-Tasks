994889896cac762a649c8f88ae93047d6ba1f5d4
Ashish Anand <ashishmax31@gmail.com>
2015-11-14 17:38:07 +0530

    removed html code inside main script and added exit condition

---
+++ batch-1/movie-info/ashish/movie_details.rb
@@ -0,0 +1,31 @@
+require 'json'
+require 'open-uri'
+puts"Hello,Enter the movie name:\n"
+usr_input=gets.chomp
+usr_input=usr_input.split(" ")# Check whether the entered movie is more than one word
+usr_input=usr_input.join("+")#If yes,add + in between the words.
+movie_url= "http://www.omdbapi.com/?t=#{usr_input}&y=&plot=short&r=json"
+content = open(movie_url).read
+content_hash=JSON.parse(content)
+if content_hash["Response"]=="False"
+	puts 'Please enter a valid movie name.'
+	exit(0)
+end
+html_file=File.new("movie_detail.html","w")
+template_add_data=File.read("template.html")
+template_read=template_add_data.split("\n")
+data=[content_hash["Title"],content_hash["Poster"],content_hash["Genre"],content_hash["Actors"],content_hash["Plot"],content_hash["imdbRating"]]
+repace_list=["{{Title}}","{{Poster}}","{{Genre}}","{{Actors}}","{{Plot}}","{{imdbRating}}"]
+template_read.each_index do |i|
+	repace_list.each_index do |j|
+		if template_read[i].include? repace_list[j]
+
+			template_add_data=template_add_data.gsub(repace_list[j],data[j])
+		end
+	end		
+end
+html_file.puts(template_add_data)	
+html_file.close
+
+
+
