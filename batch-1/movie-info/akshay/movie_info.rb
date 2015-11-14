82d840cd3a656b610a7c0cbb4cf80cf7ed22a666
aksh93 <akshaysasidharan93@gmail.com>
2015-11-14 21:56:13 +0530

    reworked from basil's code

--- batch-1/movie-info/akshay/movie_info.rb
+++ batch-1/movie-info/akshay/movie_info.rb
@@ -16,19 +16,11 @@ def generate_page (movie_name)
 	content_hash=JSON.parse(content)
 	html_file=File.new("movie_details.html","w")
 	movie_details=File.read("template.html")
-	movie_details=movie_details.gsub("{{Poster}}",content_hash["Poster"])
-	movie_details=movie_details.gsub("{{Title}}",content_hash["Title"])
-	movie_details=movie_details.gsub("{{Released}}",content_hash["Released"])
-	movie_details=movie_details.gsub("{{Genre}}",content_hash["Genre"])
-	movie_details=movie_details.gsub("{{Runtime}}",content_hash["Runtime"])
-	movie_details=movie_details.gsub("{{Language}}",content_hash["Language"])
-	movie_details=movie_details.gsub("{{Writer}}",content_hash["Writer"])
-	movie_details=movie_details.gsub("{{Actors}}",content_hash["Actors"])
-	movie_details=movie_details.gsub("{{Plot}}",content_hash["Plot"])
-	movie_details=movie_details.gsub("{{Awards}}",content_hash["Awards"])
-	movie_details=movie_details.gsub("{{imdbRating}}",content_hash["imdbRating"])
-	movie_details=movie_details.gsub("{{imdbVotes}}",content_hash["imdbVotes"])
-	movie_details=movie_details.gsub("{{Metascore}}",content_hash["Metascore"])
+	n=Array['{{Poster}}','{{Title}}','{{Released}}','{{Genre}}','{{Runtime}}','{{Language}}','{{Writer}}','{{Actors}}','{{Plot}}','{{Awards}}','{{imdbRating}}','{{imdbVotes}}','{{Metascore}}']
+	p=Array['Poster','Title','Released','Genre','Runtime','Language','Writer','Actors','Plot','Awards','imdbRating','imdbVotes','Metascore']
+	for i in 0..12
+ 	movie_details=movie_details.gsub(n[i],content_hash[p[i]])
+	end
 	html_file.puts(movie_details)
 	puts "Html page has been generated in "+Dir.getwd+" as movie_details.html"
 end
